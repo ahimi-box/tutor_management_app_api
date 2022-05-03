98f5118cd53a20afb114c338ec0d94ea02951a4e
ahimi <testtestabe07@gmail.com>
2022-05-03 22:44:39 +0900

    conf修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,15 +8,12 @@ Rails.application.routes.draw do
     namespace :v1 do
       # api test action
       resources :hello, only:[:index]
-<<<<<<< HEAD
       mount_devise_token_auth_for 'Student', at: 'auth'
 
       mount_devise_token_auth_for 'Teacher', at: 'teacher_auth'
       # as :teacher do
         # Define routes for Teacher within this block.
       # end
-=======
->>>>>>> d683ff330070dad4b1430996fd8218a72f3c94bd
     end
   end
 end
